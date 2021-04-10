248419b9c8ebb43681155858c0cab04e9a5e670b
Allen Lim (NCS) <allen.lim@ncs.com.sg>
2021-04-10 15:40:24 +0800

    First commit

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
+Amplify.configure(config);
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default function App() {
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,12 +15,6 @@ export default function App() {
     </View>
   );
 }
+export default withAuthenticator(App);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
