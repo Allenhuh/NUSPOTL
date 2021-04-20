0c0b9fb30252f5446ab38f29e5c53daed73a9062
Fareesha Sailesh Timbadia <fareeshatimbadia@gmail.com>
2021-04-20 21:32:13 +0800

    commit;20/04/2021

--- amplify_petApp/App.js
+++ amplify_petApp/App.js
@@ -1,72 +1,112 @@
-import React from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
-import AddPetScreen from './src/screens/add-pet-screen';
-import HomeScreen from './src/screens/home-screen';
-import {Button} from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {withAuthenticator} from 'aws-amplify-react-native';
-import {Auth} from 'aws-amplify';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-const App: () => React$Node = () => {
-  const Stack = createStackNavigator();
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={({navigation}) => ({
-              headerTitleStyle: { alignSelf: 'center' },
-              title: 'Welcome to POTL',
-              headerStyle: {
-                backgroundColor: '#E8D7D2',
-              },
-              headerLeft: () => (
-                <View style={styles.logOutBtn}>
-                  <Button
-                    icon={<Icon name="sign-out" size={25} color="#000000" />}
-                    onPress={ () => {
-                      Auth.signOut();
-                    }}
-                    type="clear"
-                  />
-                </View>
-              ),
-              headerRight: () => (
-                <TouchableOpacity
-                  style={styles.addButton}
-                  onPress={() => navigation.navigate('AddPet')}>
-                  <Icon name={'plus'} size={20} color="#000000" />
-                </TouchableOpacity>
-              ),
-            })}
-          />
-          <Stack.Screen
-            name="AddPet"
-            buttonStyle={styles.addButton}
-            component={AddPetScreen}
-            options={{
-              headerTitleStyle: { alignSelf: 'center' },
-              title: 'Add Pet',
-              headerStyle: {
-                backgroundColor: '#A0B3C1',
-              },
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
 };
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
 const styles = StyleSheet.create({
-  addButton: {
-    marginRight: 20,
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  logOutBtn: {
-    marginLeft: 10,
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
   },
 });
 
